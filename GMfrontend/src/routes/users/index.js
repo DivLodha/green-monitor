cb2c06e137c9ba95927de4ca8832a562d8dd9233
Lamiaa Hafez <lamia@Lamia.local>
2018-08-08 19:14:32 +0400

    frontend

---
+++ GMfrontend/src/routes/users/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./login";
+import { isLoggedIn } from "../../helpers/authentication";
+import Dashboard from "../dashboard";
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route
+          path=""
+          render={props => {
+            if (isLoggedIn()) {
+              return <Dashboard />;
+            }
+            return <Login />;
+          }}
+        />
+      </Switch>
+    );
+  }
+}
+
+export default App;
