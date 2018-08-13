e232bbfd754e9c207b6a2a4f59f2e6fa545190f0
Abeer Fatima <abeer.smiles@gmail.com>
2018-08-13 18:53:05 +0400

    appbar/header updated

--- GMfrontend/src/components/Layout.js
+++ GMfrontend/src/components/Layout.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Header from "./Header";
 import "./bootstrap.min.css";
 import "./app.css";
+import SimpleAppBar from "./SimpleAppBar";
 class Layout extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <SimpleAppBar />
         <div className="container">{this.props.children}</div>
       </div>
     );
--- GMfrontend/src/components/SimpleAppBar.js
+++ GMfrontend/src/components/SimpleAppBar.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Button, IconButton} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  
+};
 class SimpleAppBar extends Component {
   render() {
     return (
+      <div className={styles.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="headline" gutterBottom color="inherit">
+        {/* <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="headline" className={styles.flex} color="inherit" style={{ flex: 1 }}>
             Green Monitor
           </Typography>
+          <div>
+          <Button color="inherit" href='/about'>About</Button>
+          <Button color="inherit" href='/users/'>Dashboard</Button>
+          </div>
         </Toolbar>
       </AppBar>
+      </div>
     );
   }
 }
